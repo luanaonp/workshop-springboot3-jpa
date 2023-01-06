3423a3f861913470bf7f8ce6c00dbbdec19655a8
Luana <luanaonp@gmail.com>
2023-01-06 19:45:18 -0300

    Project created

---
+++ src/test/java/com/educandweb/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandweb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
