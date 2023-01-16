98f50a432411fb4d237d0a21dd1902e06d27cbad
Luana <luanaonp@gmail.com>
2023-01-16 20:56:34 -0300

    JPA repository dependency injection, database seeding

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
 
-@RestController
+@RestController 
 @RequestMapping(value="/users")
 public class UserResource {
 	
