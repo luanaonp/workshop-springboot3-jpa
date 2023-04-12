bb8c5b52122ba1ad1f5927045d9df546e7be3048
Luana <luanaonp@gmail.com>
2023-04-11 23:10:23 -0300

    Exception handling - Delete

--- src/main/java/com/educandoweb/course/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course.services.exception.DataBaseException;
 import com.educandoweb.course.services.exception.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,5 +25,18 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 		
 	}
+	
+
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request){
+		
+		String error = "Database error ";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
 
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exception.DataBaseException;
 import com.educandoweb.course.services.exception.ResourceNotFoundException;
 import com.educandoweb.course.entities.User;
 
@@ -31,7 +34,13 @@ public class UserService {
 	  }
 	  
 	  public void delete(Long id){
+		  try {
 		  repository.deleteById(id);
+		  }  catch (EmptyResultDataAccessException e) {
+			  throw new ResourceNotFoundException(id);
+		  } catch(DataIntegrityViolationException e) {
+			  throw new DataBaseException(e.getMessage());
+		  }
 	  }
 	  
 	 public User update(Long id, User obj) {
