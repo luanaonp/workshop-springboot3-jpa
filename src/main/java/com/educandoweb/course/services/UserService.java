cd3962732fb2362b9ae16eddd4a1ed0a5e9ab741
Luana <luanaonp@gmail.com>
2023-04-12 09:17:02 -0300

    Exception handling - Update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exception.DataBaseException;
 import com.educandoweb.course.services.exception.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
 import com.educandoweb.course.entities.User;
 
 @Service
@@ -44,9 +47,14 @@ public class UserService {
 	  }
 	  
 	 public User update(Long id, User obj) {
-		 User entity = repository.getReferenceById(id);
-		 updateData(entity,obj);
-		 return repository.save(entity);
+		  try {
+		  User entity = repository.getReferenceById(id);
+		  updateData(entity,obj);
+		  return repository.save(entity);
+		  }catch(EntityNotFoundException e ) {
+			  throw new ResourceNotFoundException(id);
+		  }
+		
 	 }
 
 	private void updateData(User entity, User obj) {
